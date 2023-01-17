bb3c981959d9cfbe4389e4f53a970e1a396e800d
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-01-17 14:56:53 +0530

    created navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react'
+
+export default function Navbar() {
+    const [navbarOpen, setNavbarOpen] = useState(false);
+  return (
+           <div className="navbar">
+              <button className="hamburger">&#9776;</button>
+              <ul className="menu">
+                  
+              </ul>
+           </div>
+  
+
+  )
+}
